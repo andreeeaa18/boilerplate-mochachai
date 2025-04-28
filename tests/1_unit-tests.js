e3647a53c5b2b92b8d4ce13113c9e62bc675d5e6
andreeeaa18 <andreevandreea.18@gmail.com>
2025-04-28 13:14:48 +0300

    Second try #12

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -117,7 +117,7 @@ suite("Unit Tests", function () {
     // #12
     test("Array #include, #notInclude", function () {
       assert.notInclude(winterMonths, "jul", "It's summer in july...");
-      assert.notInclude(
+      assert.include(
         backendLanguages,
         "javascript",
         "JS is a backend language"
