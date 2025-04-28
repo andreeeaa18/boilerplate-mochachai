1db4e3bc56bda5d171f5d75f1ba137f8b3b68d7d
andreeeaa18 <andreevandreea.18@gmail.com>
2025-04-28 12:50:51 +0300

    Second try #8

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -85,7 +85,7 @@ suite("Unit Tests", function () {
       assert.isAtMost("hello".length, 5);
       assert.isAbove(1, 0);
       assert.isAtMost(Math.PI, 3);
-      assert.isAbove(1 - Math.random(), 1);
+      assert.isAtMost(1 - Math.random(), 1);
     });
     // #9
     test("#isBelow, #isAtLeast", function () {
